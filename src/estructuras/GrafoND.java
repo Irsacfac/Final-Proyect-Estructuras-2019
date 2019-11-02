5f80dd9f48561036beb961cc028feda87a0101b1
Isaac Ortega Arguedas <irsacfac@gmail.com>
2019-11-02 15:45:46 -0600

    Se agregaron metodos

--- src/estructuras/GrafoND.java
+++ src/estructuras/GrafoND.java
@@ -9,6 +9,16 @@ public class GrafoND<T> {
 		nodos = new Vector<>();
 	}
 	
+	public void agregarNodo(NodoG<T> pNodo) {
+		if(!nodos.contains(pNodo)) {
+			nodos.add(pNodo);
+		}
+	}
+	
+	public void agregarArco(NodoG<T> pNodo1, NodoG<T> pNodo2) {
+		pNodo1.agregarArco(pNodo2);
+	}
+	
 	public void warshall() {
 		
 	}
