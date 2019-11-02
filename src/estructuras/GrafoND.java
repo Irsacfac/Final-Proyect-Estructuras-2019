283ba3d5b1d5789437b53ae59434ee27d87972eb
Isaac Ortega Arguedas <irsacfac@gmail.com>
2019-11-02 15:42:09 -0600

    e agregaro metodos en GrafoND y NodoG

--- src/estructuras/GrafoND.java
+++ src/estructuras/GrafoND.java
@@ -1,5 +1,27 @@
 package estructuras;
 
-public class GrafoND {
+import java.util.Vector;
 
+public class GrafoND<T> {
+	private Vector<NodoG<T>> nodos;
+	
+	public GrafoND() {
+		nodos = new Vector<>();
+	}
+	
+	public void warshall() {
+		
+	}
+	
+	public void floyd() {
+		
+	}
+	
+	public void MST() {
+		
+	}
+	
+	public void dijkstra() {
+		
+	}
 }
--- src/estructuras/NodoG.java
+++ src/estructuras/NodoG.java
@@ -1,5 +1,40 @@
 package estructuras;
 
-public class NodoG {
+import java.util.Vector;
 
+public class NodoG <T>{
+	private T elemento;
+	private boolean visitado;
+	private int peso;
+	private Vector<NodoG<T>> arcos;
+	
+	public NodoG(){
+		arcos = new Vector<>();
+	}
+	
+	public void agregarArco(NodoG<T> pNodo) {
+		if(!arcos.contains(pNodo)) {
+			arcos.add(pNodo);
+			if(!pNodo.getArcos().contains(this)) {
+				pNodo.getArcos().add(this);
+			}
+			
+		}
+	}
+	
+	public Vector<NodoG<T>> getArcos(){
+		return arcos;
+	}
+	
+	protected void setVisitado(boolean pVisitado) {
+		visitado = pVisitado;
+	}
+	
+	public boolean isVisitado() {
+		return visitado;
+	}
+	
+	public T getElemento() {
+		return elemento;
+	}
 }
