0fe55a4c52312f46afa8996e6d57a18349952e33
Isaac Ortega Arguedas <irsacfac@gmail.com>
2019-11-21 17:11:04 -0600

    Auto stash before merge of "master" and "origin/master"

--- src/estructuras/AlgoritmosDeBusqueda/Subclasses/ElementoDeMatriz.java
+++ src/estructuras/AlgoritmosDeBusqueda/Subclasses/ElementoDeMatriz.java
@@ -3,9 +3,10 @@ package estructuras.AlgoritmosDeBusqueda.Subclasses;
 import estructuras.Arco;
 import estructuras.NodoG;
 
-public class ElementoDeMatriz {
-    private NodoG nodoOrigen;
-    private NodoG nodoDestino;
+public class ElementoDeMatriz<T> {
+    private NodoG<T> nodoOrigen;
+    private NodoG<T> nodoDestino;
+    private NodoG<T> nodoAnterior;
 
     public ElementoDeMatriz(NodoG pNodoOrigen, NodoG pNodoDestino) {
         this.nodoOrigen = pNodoOrigen;
