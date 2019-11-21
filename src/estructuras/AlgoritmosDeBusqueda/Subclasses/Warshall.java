06ee08f8cbd24e4705c9510f3fdd723bda9f4241
Isaac Ortega Arguedas <irsacfac@gmail.com>
2019-11-21 01:22:18 -0600

    Cambios Warshall
    Se empezo a utilizar un vector para intentar realizar la busqueda

--- src/estructuras/AlgoritmosDeBusqueda/Subclasses/Warshall.java
+++ src/estructuras/AlgoritmosDeBusqueda/Subclasses/Warshall.java
@@ -7,6 +7,7 @@ import estructuras.NodoG;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Vector;
 
 public class Warshall<T> extends AlgoritmoDeBusqueda {
 
@@ -14,12 +15,14 @@ public class Warshall<T> extends AlgoritmoDeBusqueda {
 	private GrafoND<T> grafo;
 	private NodoG<T> nodoParada;
 	private ElementoDeMatriz<T> ruta;
+	private Vector<Vector<ElementoDeMatriz<T>>> recorrido;
 	
 	public Warshall(GrafoND<T> pGrafo, NodoG<T> pPartida, NodoG<T> pDestino) {
 		grafo = pGrafo;
 		ruta = new ElementoDeMatriz<>(pPartida, pDestino);
 		nodoParada = new NodoG<>(null);
 		tablaHash = new Hashtable<>();
+		recorrido = new Vector<>();
 		
 		this.arcosDirectos();
 		this.buscarElemento();
@@ -35,11 +38,13 @@ public class Warshall<T> extends AlgoritmoDeBusqueda {
 	private void arcosDirectos() {
 		NodoG<T> nodoActual;
 		ElementoDeMatriz<T> elementoActual;
+		recorrido.add(new Vector<>());
 		for(int posNodo = 0; posNodo < grafo.getNodos().size(); posNodo++) {
 			nodoActual = grafo.getNodos().get(posNodo);
 			for(int conecciones = 0; conecciones < nodoActual.getArcos().size(); conecciones++) {
 				elementoActual = new ElementoDeMatriz<>(nodoActual, nodoActual.getArcos().get(conecciones));
 				tablaHash.put(elementoActual, elementoActual);
+				recorrido.firstElement().add(elementoActual);
 				if(elementoActual.compareTo(ruta)==0) {
 					ruta = elementoActual; 
 					return;
@@ -53,10 +58,34 @@ public class Warshall<T> extends AlgoritmoDeBusqueda {
 	private void buscarElemento() {
 		NodoG<T> nodoActual;
 		NodoG<T> nodoConector;
-		ElementoDeMatriz<T> elementoActual;
+		ElementoDeMatriz<T> elementoActual = recorrido.firstElement().firstElement();
 		ElementoDeMatriz<T> origenActual;
 		ElementoDeMatriz<T> actualDestino;
-		for(int posNodo = 0; posNodo < grafo.getNodos().size(); posNodo++) {
+		int elementos = 0;
+		Vector<ElementoDeMatriz<T>> previo;
+		Vector<ElementoDeMatriz<T>> actual;
+		while(elementoActual.compareTo(ruta) != 0) {
+			recorrido.add(new Vector<>());
+			previo = recorrido.elementAt(elementos);
+			actual = recorrido.lastElement();
+			elementos++;
+			for(int pos = 0; pos<previo.size();pos++) {
+				for(int element =0; element<previo.size();element++) {
+					origenActual = previo.elementAt(pos);
+					actualDestino = previo.elementAt(element);
+					if((elementos == 1) && (origenActual.getNodoDestino() == actualDestino.getNodoOrigen())) {
+						elementoActual = new ElementoDeMatriz<>(origenActual.getNodoOrigen(),actualDestino.getNodoDestino());
+						elementoActual.setNodoAnterior(actualDestino.getNodoOrigen());
+						actual.add(elementoActual);
+					}else if(origenActual.getNodoDestino() == actualDestino.getNodoAnterior()) {
+						elementoActual = new ElementoDeMatriz<>(origenActual.getNodoOrigen(),actualDestino.getNodoDestino());
+						elementoActual.setNodoAnterior(actualDestino.getNodoOrigen());
+						actual.add(elementoActual);
+					}
+				}
+			}
+		}
+		/*for(int posNodo = 0; posNodo < grafo.getNodos().size(); posNodo++) {
 			nodoConector = grafo.getNodos().get(posNodo);
 			for(int arcoActual = 0; arcoActual< nodoConector.getArcos().size();arcoActual++) {
 				nodoActual = nodoConector.getArcos().get(arcoActual);
@@ -73,10 +102,10 @@ public class Warshall<T> extends AlgoritmoDeBusqueda {
 				}
 				/*if(tablaHash.containsKey(ruta)) {
 					return;
-				}*/
+				}
 			}
 			
-		}
+		}*/
 	}
 	
 	// devuelve un array con los nodos para llegar del punto a al punto b, se debe recorrer alreves
